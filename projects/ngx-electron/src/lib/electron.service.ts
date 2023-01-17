dd4f0936cd3fe451bc2e8243506b8a5a9f057601
Simon W <MrWeezle@users.noreply.github.com>
2023-01-17 15:09:27 +0100

    Update electron.service.ts

--- projects/ngx-electron/src/lib/electron.service.ts
+++ projects/ngx-electron/src/lib/electron.service.ts
@@ -87,4 +87,8 @@ export class ElectronService {
     public get shell(): Electron.Shell {
         return this.electron ? this.electron.shell : null;
     }
+    
+    public get app(): Electron.App {
+        return this.electorn ? this.electron.app : null;
+    }
 }
